c688b4b7da0eb26a9abe19d4a3b8c38dbd2cb90a
DESKTOP-T85G3V9\eleonora <leo1994448@gmail.com>
2017-03-23 20:05:49 +0000

    Foto searchParty.jpg para la pantalla content_search

---
+++ HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/search/SearchView.java
@@ -0,0 +1,53 @@
+package es.ulpgc.eite.clean.mvp.sample.search;
+
+import es.ulpgc.eite.clean.mvp.GenericActivity;
+import es.ulpgc.eite.clean.mvp.sample.hangapp.HangApp;
+import es.ulpgc.eite.clean.mvp.sample.hangapp.HangAppPresenter;
+
+/**
+ * Created by eleonora on 23/03/2017.
+ */
+
+public class SearchView  extends GenericActivity<HangApp.PresenterToView, HangApp.ViewToPresenter, HangAppPresenter>
+        implements HangApp.PresenterToView  {
+private ImageView im
+
+    @Override
+    public void finishScreen() {
+
+    }
+
+    @Override
+    public void hideToolbar() {
+
+    }
+
+    @Override
+    public void hideText() {
+
+    }
+
+    @Override
+    public void showText() {
+
+    }
+
+    @Override
+    public void setText(String txt) {
+
+    }
+
+    @Override
+    public void setLabel(String txt) {
+
+    }
+
+    @Override
+    public void setLabelSearch(String txt) {
+
+    }
+
+    private
+
+
+}
