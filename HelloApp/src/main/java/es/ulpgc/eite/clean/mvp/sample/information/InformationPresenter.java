3bdcdd4f3cea26c1f2d440db583ea245399aabd1
leo123444 <leo1994448@gmail.com>
2017-03-31 18:21:24 +0000

    renombrado el activity

--- HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/information/InformationPresenter.java
+++ HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/information/InformationPresenter.java
@@ -1,8 +1,47 @@
 package es.ulpgc.eite.clean.mvp.sample.information;
 
+import android.app.Application;
+import android.content.Context;
+
+import es.ulpgc.eite.clean.mvp.Presenter;
+
 /**
  * Created by alumno on 31/03/2017.
  */
 
-class InformationPresenter {
+class InformationPresenter implements Presenter<Information.PresenterToView> {
+    @Override
+    public void onCreate(Information.PresenterToView presenterToView) {
+
+    }
+
+    @Override
+    public void onResume(Information.PresenterToView presenterToView) {
+
+    }
+
+    @Override
+    public void onDestroy(boolean b) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    @Override
+    public Application getApplication() {
+        return null;
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return null;
+    }
+
+    @Override
+    public Context getActivityContext() {
+        return null;
+    }
 }
--- HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/information/InformationView.java
+++ HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/information/InformationView.java
@@ -1,6 +1,17 @@
 package es.ulpgc.eite.clean.mvp.sample.information;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import es.ulpgc.eite.clean.mvp.GenericActivity;
+import es.ulpgc.eite.clean.mvp.sample.R;
 import es.ulpgc.eite.clean.mvp.sample.search.Search;
 import es.ulpgc.eite.clean.mvp.sample.search.SearchPresenter;
 
@@ -10,4 +21,23 @@ import es.ulpgc.eite.clean.mvp.sample.search.SearchPresenter;
 
 public class InformationView extends GenericActivity<Information.PresenterToView, Information.ViewToPresenter, InformationPresenter>
         implements Information.PresenterToView  {
+    ImageView image;
+    TextView textInformation;
+    TextView numberOfParticipants;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_);
+
+
+
+
+    }
+
+
+
+
+
+
 }
