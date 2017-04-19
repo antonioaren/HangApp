0c46feb640582645e4df3126a4ab4bea8913b827
DESKTOP-T85G3V9\eleonora <leo1994448@gmail.com>
2017-04-19 18:07:55 +0100

    creada la clase que representa activity_detail y declarada en el AndroidManifest.xml

--- HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/detail/DetailView.java
+++ HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/detail/DetailView.java
@@ -1,13 +1,22 @@
 package es.ulpgc.eite.clean.mvp.sample.detail;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import es.ulpgc.eite.clean.mvp.GenericActivity;
-import es.ulpgc.eite.clean.mvp.sample.hangapp.HangApp;
-import es.ulpgc.eite.clean.mvp.sample.hangapp.HangAppPresenter;
+import es.ulpgc.eite.clean.mvp.sample.R;
 
 /**
  * Created by eleonora on 19/04/2017.
  */
 
 public class DetailView  extends GenericActivity<Detail.PresenterToView, Detail.ViewToPresenter, DetailPresenter>
-        implements Detail.PresenterToView  {
+        implements Detail.PresenterToView {
+TextView detail;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        detail=(TextView)findViewById(R.id.detail);
+    }
 }
