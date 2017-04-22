aa2d410ca739307a49162bb4357ef90d667f2006
antonioaren <antonioaren@hotmail.com>
2017-04-22 01:17:08 +0100

    Commited to pull and update all code

--- HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/hangapp/HangAppPresenter.java
+++ HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/hangapp/HangAppPresenter.java
@@ -15,200 +15,201 @@ import es.ulpgc.eite.clean.mvp.sample.search.SearchView;
 import static android.content.ContentValues.TAG;
 
 public class HangAppPresenter extends GenericPresenter
-          <HangApp.PresenterToView, HangApp.PresenterToModel, HangApp.ModelToPresenter, HangAppModel>
-          implements HangApp.ViewToPresenter, HangApp.ModelToPresenter, HangApp.DummyTo, HangApp.ToDummy, HangApp.SearchTo, HangApp.ToHangApp, HangApp.HangAppToAdd, HangApp.AddTo {
+        <HangApp.PresenterToView, HangApp.PresenterToModel, HangApp.ModelToPresenter, HangAppModel>
+        implements HangApp.ViewToPresenter, HangApp.ModelToPresenter, HangApp.DummyTo, HangApp.ToDummy, HangApp.SearchTo, HangApp.ToHangApp, HangApp.HangAppToAdd, HangApp.AddTo {
 
-  //Actualizando cosas
-  private boolean toolbarVisible;
-  private boolean buttonClicked;
-  private boolean textVisible;
-  private boolean imageVisibility;
-  private boolean selectorsVisible;
-  /**
-   * Operation called during VIEW creation in {@link GenericActivity#onResume(Class, Object)}
-   * Responsible to initialize MODEL.
-   * Always call {@link GenericPresenter#onCreate(Class, Object)} to initialize the object
-   * Always call  {@link GenericPresenter#setView(ContextView)} to save a PresenterToView reference
-   *
-   * @param view The current VIEW instance
-   */
-  @Override
-  public void onCreate(HangApp.PresenterToView view) {
-    super.onCreate(HangAppModel.class, this);
-    setView(view);
-    Log.d(TAG, "calling onCreate()");
+    //Actualizando cosas
+    private boolean toolbarVisible;
+    private boolean buttonClicked;
+    private boolean textVisible;
+    private boolean imageVisibility;
+    private boolean selectorsVisible;
 
-    Log.d(TAG, "calling startingMainScreen()");
-    Mediator app = (Mediator) getView().getApplication();
-    app.startingHangAppScreen(this);
-  }
+    /**
+     * Operation called during VIEW creation in {@link GenericActivity#onResume(Class, Object)}
+     * Responsible to initialize MODEL.
+     * Always call {@link GenericPresenter#onCreate(Class, Object)} to initialize the object
+     * Always call  {@link GenericPresenter#setView(ContextView)} to save a PresenterToView reference
+     *
+     * @param view The current VIEW instance
+     */
+    @Override
+    public void onCreate(HangApp.PresenterToView view) {
+        super.onCreate(HangAppModel.class, this);
+        setView(view);
+        Log.d(TAG, "calling onCreate()");
 
-  /**
-   * Operation called by VIEW after its reconstruction.
-   * Always call {@link GenericPresenter#setView(ContextView)}
-   * to save the new instance of PresenterToView
-   *
-   * @param view The current VIEW instance
-   */
-  @Override
-  public void onResume(HangApp.PresenterToView view) {
-    setView(view);
-    Log.d(TAG, "calling onResume()");
-
-
-    if(configurationChangeOccurred()) {
-      getView().setLabel(getModel().getSearchLabel());
-      getView().setLabel(getModel().getSearchLabel());
-
-      checkToolbarVisibility();
-      checkTextVisibility();
-
-      if (buttonClicked) {
-        getView().setText(getModel().getText());
-      }
+        Log.d(TAG, "calling startingMainScreen()");
+        Mediator app = (Mediator) getView().getApplication();
+        app.startingHangAppScreen(this);
     }
 
-  }
-
-  /**
-   * Helper method to inform Presenter that a onBackPressed event occurred
-   * Called by {@link GenericActivity}
-   */
-  @Override
-  public void onBackPressed() {
-    Log.d(TAG, "calling onBackPressed()");
-
-  }
-
-  /**
-   * Hook method called when the VIEW is being destroyed or
-   * having its configuration changed.
-   * Responsible to maintain MVP synchronized with Activity lifecycle.
-   * Called by onDestroy methods of the VIEW layer, like: {@link GenericActivity#onDestroy()}
-   *
-   * @param isChangingConfiguration true: configuration changing & false: being destroyed
-   */
-  @Override
-  public void onDestroy(boolean isChangingConfiguration) {
-    super.onDestroy(isChangingConfiguration);
-    Log.d(TAG, "calling onDestroy()");
-  }
+    /**
+     * Operation called by VIEW after its reconstruction.
+     * Always call {@link GenericPresenter#setView(ContextView)}
+     * to save the new instance of PresenterToView
+     *
+     * @param view The current VIEW instance
+     */
+    @Override
+    public void onResume(HangApp.PresenterToView view) {
+        setView(view);
+        Log.d(TAG, "calling onResume()");
 
 
-  ///////////////////////////////////////////////////////////////////////////////////
-  // View To Presenter /////////////////////////////////////////////////////////////
+        if (configurationChangeOccurred()) {
+            getView().setLabel(getModel().getSearchLabel());
+            getView().setLabel(getModel().getSearchLabel());
+
+            checkToolbarVisibility();
+            checkTextVisibility();
+
+            if (buttonClicked) {
+                getView().setText(getModel().getText());
+            }
+        }
+
+    }
+
+    /**
+     * Helper method to inform Presenter that a onBackPressed event occurred
+     * Called by {@link GenericActivity}
+     */
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG, "calling onBackPressed()");
+
+    }
+
+    /**
+     * Hook method called when the VIEW is being destroyed or
+     * having its configuration changed.
+     * Responsible to maintain MVP synchronized with Activity lifecycle.
+     * Called by onDestroy methods of the VIEW layer, like: {@link GenericActivity#onDestroy()}
+     *
+     * @param isChangingConfiguration true: configuration changing & false: being destroyed
+     */
+    @Override
+    public void onDestroy(boolean isChangingConfiguration) {
+        super.onDestroy(isChangingConfiguration);
+        Log.d(TAG, "calling onDestroy()");
+    }
 
 
-  @Override
-  public void onButtonSearchClicked() {
-      Log.d(TAG, "calling onButtonSearchClicked()");
+    ///////////////////////////////////////////////////////////////////////////////////
+    // View To Presenter /////////////////////////////////////////////////////////////
+
+
+    @Override
+    public void onButtonSearchClicked() {
+        Log.d(TAG, "calling onButtonSearchClicked()");
 //      if (isViewRunning()) {
 //          getModel().onChangeMsgByBtnClicked();
 //          getView().setText(getModel().getText());
 //          textVisible = true;
 //          buttonClicked = true;
 
-       Navigator app = (Navigator) getView().getApplication();
-      app.goToSearchScreen(this);
-      }
+        Navigator app = (Navigator) getView().getApplication();
+        app.goToSearchScreen(this);
+    }
     //  checkTextVisibility();
 
-//Metodo ya funciona
-      @Override
-      public void onButtonAddClicked() {
-        Log.d(TAG,"calling onButtonAddClicked()");
+    //Metodo ya funciona
+    @Override
+    public void onButtonAddClicked() {
+        Log.d(TAG, "calling onButtonAddClicked()");
         Navigator app = (Navigator) getView().getApplication();
         app.goToAddScreen(this);
-      }
-
-
-      ///////////////////////////////////////////////////////////////////////////////////
-  // To HangApp //////////////////////////////////////////////////////////////////////
-
-  @Override
-  public void onScreenStarted() {
-    Log.d(TAG, "calling onScreenStarted()");
-    if(isViewRunning()) {
-      getView().setLabel(getModel().getSearchLabel());
-      getView().setLabel(getModel().getAddLabel());
-    }
-    checkToolbarVisibility();
-    checkTextVisibility();
-  }
-
-  @Override
-  public void setToolbarVisibility(boolean visible) {
-    toolbarVisible = visible;
-  }
-
-  @Override
-  public void setTextVisibility(boolean visible) {
-    textVisible = visible;
-  }
-
-  @Override
-  public void setImageVisibility(boolean ImageVisibility) {
-     imageVisibility=ImageVisibility;
-  }
-
-
-
-
-  ///////////////////////////////////////////////////////////////////////////////////
-  // HangApp To //////////////////////////////////////////////////////////////////////
-
-
-  @Override
-  public Context getManagedContext(){
-    return getActivityContext();
-  }
-
-  @Override
-  public void destroyView(){
-    if(isViewRunning()) {
-      getView().finishScreen();
-    }
-  }
-  @Override
-  public boolean isToolbarVisible() {
-    return toolbarVisible;
-  }
-
-  @Override
-  public boolean isTextVisible() {
-    return textVisible;
-  }
-
-  @Override
-  public boolean isSelectorsVisible() {
-    return selectorsVisible;
-  }
-@Override
-public boolean isImageVisible(){
-  return imageVisibility;
-}
-
-  ///////////////////////////////////////////////////////////////////////////////////
-
-  private void checkToolbarVisibility(){
-    Log.d(TAG, "calling checkToolbarVisibility()");
-    if(isViewRunning()) {
-      if (!toolbarVisible) {
-        getView().hideToolbar();
-      }
     }
 
-  }
 
-  private void checkTextVisibility(){
-    Log.d(TAG, "calling checkTextVisibility()");
-    if(isViewRunning()) {
-      if(!textVisible) {
-        getView().hideText();
-      } else {
-        getView().showText();
-      }
+    ///////////////////////////////////////////////////////////////////////////////////
+    // To HangApp //////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void onScreenStarted() {
+        Log.d(TAG, "calling onScreenStarted()");
+        if (isViewRunning()) {
+            getView().setLabel(getModel().getSearchLabel());
+            getView().setLabel(getModel().getAddLabel());
+        }
+        checkToolbarVisibility();
+        checkTextVisibility();
     }
-  }
+
+    @Override
+    public void setToolbarVisibility(boolean visible) {
+        toolbarVisible = visible;
+    }
+
+    @Override
+    public void setTextVisibility(boolean visible) {
+        textVisible = visible;
+    }
+
+    @Override
+    public void setImageVisibility(boolean ImageVisibility) {
+        imageVisibility = ImageVisibility;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // HangApp To //////////////////////////////////////////////////////////////////////
+
+
+    @Override
+    public Context getManagedContext() {
+        return getActivityContext();
+    }
+
+    @Override
+    public void destroyView() {
+        if (isViewRunning()) {
+            getView().finishScreen();
+        }
+    }
+
+    @Override
+    public boolean isToolbarVisible() {
+        return toolbarVisible;
+    }
+
+    @Override
+    public boolean isTextVisible() {
+        return textVisible;
+    }
+
+    @Override
+    public boolean isSelectorsVisible() {
+        return selectorsVisible;
+    }
+
+    @Override
+    public boolean isImageVisible() {
+        return imageVisibility;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    private void checkToolbarVisibility() {
+        Log.d(TAG, "calling checkToolbarVisibility()");
+        if (isViewRunning()) {
+            if (!toolbarVisible) {
+                getView().hideToolbar();
+            }
+        }
+
+    }
+
+    private void checkTextVisibility() {
+        Log.d(TAG, "calling checkTextVisibility()");
+        if (isViewRunning()) {
+            if (!textVisible) {
+                getView().hideText();
+            } else {
+                getView().showText();
+            }
+        }
+    }
 
 }
--- HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/hangapp/HangAppView.java
+++ HelloApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/hangapp/HangAppView.java
@@ -1,5 +1,6 @@
 package es.ulpgc.eite.clean.mvp.sample.hangapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,116 +11,116 @@ import es.ulpgc.eite.clean.mvp.GenericActivity;
 import es.ulpgc.eite.clean.mvp.sample.R;
 
 public class HangAppView
-    extends GenericActivity<HangApp.PresenterToView, HangApp.ViewToPresenter, HangAppPresenter>
-    implements HangApp.PresenterToView {
+        extends GenericActivity<HangApp.PresenterToView, HangApp.ViewToPresenter, HangAppPresenter>
+        implements HangApp.PresenterToView {
 
-  private Toolbar toolbar;
-  private Button buttonSearch;
-  private Button buttonAdd;
-  private TextView text;
+    private Toolbar toolbar;
+    private Button buttonSearch;
+    private Button buttonAdd;
+    private TextView text;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_hangapp);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangapp);
 
-    text = (TextView) findViewById(R.id.textView7);
+        text = (TextView) findViewById(R.id.textView7);
 
-    toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    buttonSearch = (Button) findViewById(R.id.buttonSearch);
-    buttonSearch.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        getPresenter().onButtonSearchClicked();
-      }
-    });
-    buttonAdd = (Button) findViewById(R.id.buttonAdd);
-    buttonAdd.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        getPresenter().onButtonAddClicked();
-      }
-    });
-  }
-
-  /**
-   * Method that initialized MVP objects
-   * {@link super#onResume(Class, Object)} should always be called
-   */
-
-  @Override
-  protected void onResume() {
-    super.onResume(HangAppPresenter.class, this);
-  }
-
-  /*
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(es.ulpgc.eite.clean.mvp.sample.hangapp.R.menu.menu_dummy, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == es.ulpgc.eite.clean.mvp.sample.hangapp.R.id.action_settings) {
-      return true;
+        buttonSearch = (Button) findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPresenter().onButtonSearchClicked();
+            }
+        });
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPresenter().onButtonAddClicked();
+            }
+        });
     }
 
-    return super.onOptionsItemSelected(item);
-  }
-  */
+    /**
+     * Method that initialized MVP objects
+     * {@link super#onResume(Class, Object)} should always be called
+     */
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onResume() {
+        super.onResume(HangAppPresenter.class, this);
+    }
+
+//  /*
+//  @Override
+//  public boolean onCreateOptionsMenu(Menu menu) {
+//    // Inflate the menu; this adds items to the action bar if it is present.
+//    getMenuInflater().inflate(es.ulpgc.eite.clean.mvp.sample.hangapp.R.menu.menu_dummy, menu);
+//    return true;
+//  }
+//
+//  @Override
+//  public boolean onOptionsItemSelected(MenuItem item) {
+//    // Handle action bar item clicks here. The action bar will
+//    // automatically handle clicks on the Home/Up button, so long
+//    // as you specify a parent activity in AndroidManifest.xml.
+//    int id = item.getItemId();
+//
+//    //noinspection SimplifiableIfStatement
+//    if (id == es.ulpgc.eite.clean.mvp.sample.hangapp.R.id.action_settings) {
+//      return true;
+//    }
+//
+//    return super.onOptionsItemSelected(item);
+//  }
+//  */
 
 
-  ///////////////////////////////////////////////////////////////////////////////////
-  // Presenter To View /////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+    // Presenter To View /////////////////////////////////////////////////////////////
 
-  @Override
-  public void finishScreen() {
-    finish();
-  }
+    @Override
+    public void finishScreen() {
+        finish();
+    }
 
-  @Override
-  public void hideToolbar() {
-    toolbar.setVisibility(View.GONE);
-  }
+    @Override
+    public void hideToolbar() {
+        toolbar.setVisibility(View.GONE);
+    }
 
-  @Override
-  public void hideText() {
-    text.setVisibility(View.GONE);
-  }
+    @Override
+    public void hideText() {
+        text.setVisibility(View.GONE);
+    }
 
-  @Override
-  public void showText() {
-    text.setVisibility(View.VISIBLE);
-  }
+    @Override
+    public void showText() {
+        text.setVisibility(View.VISIBLE);
+    }
 
-  @Override
-  public void setText(String txt) {
-    text.setText(txt);
-  }
+    @Override
+    public void setText(String txt) {
+        text.setText(txt);
+    }
 
-  @Override
-  public void setLabel(String txt) {
+    @Override
+    public void setLabel(String txt) {
+    }
 
-  }
+    @Override
+    public void setLabelSearch(String txt) {
+        buttonSearch.setText(txt);
+    }
 
-  @Override
-  public void setLabelSearch(String txt) {
-    buttonSearch.setText(txt);
-  }
-
-@Override
-public void setAddLabel(String txt) {
-  buttonAdd.setText(txt);
-}
+    @Override
+    public void setAddLabel(String txt) {
+        buttonAdd.setText(txt);
+    }
 }
 
