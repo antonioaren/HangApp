7e71d1986b3c9ef34fda39321b40548bc0d6b8a0
DESKTOP-T85G3V9\eleonora <leo1994448@gmail.com>
2017-05-26 18:48:58 +0100

    creados test para insertar un nombre en la lista principal desde la app y otro para que inserte desde la base de datos.Ambos funcionan

--- HangUp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/EspressoTest.java
+++ HangUp/src/androidTest/java/es/ulpgc/eite/clean/mvp/sample/EspressoTest.java
@@ -1,67 +1,130 @@
 package es.ulpgc.eite.clean.mvp.sample;
 
 import android.support.test.espresso.ViewAssertion;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import es.ulpgc.eite.clean.mvp.sample.category.CategoryModel;
 import es.ulpgc.eite.clean.mvp.sample.category.CategoryView;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by eleonora on 24/05/2017.
  */
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest  {
+    private static final String MESSAGE = "This is a test";
+    private static final String PACKAGE_NAME = "es.ulpgc.eite.clean.mvp.sample";
 
     @Rule public final ActivityTestRule<CategoryView> presenter= new ActivityTestRule<>(CategoryView.class);
+    @Before
+    public void setup() {
+
+    }
+
+
     @Test
     public void shouldBeAbleToLaunchMainScreen(){
 //        onView(withId(R.id.buttonAdd)).perform(click());
 //         onView(withId(R.id.textName)).check(matches(ViewMatchers.isDisplayed()));
        // onView(withId(R.id.title)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.title)).check((ViewAssertion) ViewMatchers.isDisplayed());
+        onView(withId(R.id.title)).check((ViewAssertion) isDisplayed());
     }
      @Test
     public void testOnAddClickedFromMainScreen(){
          onView(withId(R.id.buttonAdd)).perform(click());
      //actualizando test
-         onView(withId(R.id.textPhoto)).check(matches(ViewMatchers.isDisplayed()));
-         onView(withId(R.id.textName)).check(matches(ViewMatchers.isDisplayed()));
-         onView(withId(R.id.radioGroup)).check(matches(ViewMatchers.isDisplayed()));
-         onView(withId(R.id.content_name)).check(matches(ViewMatchers.isDisplayed()));
-         onView(withId(R.id.buttonAdd)).check(matches(ViewMatchers.isDisplayed()));
+         onView(withId(R.id.textPhoto)).check(matches(isDisplayed()));
+         onView(withId(R.id.textName)).check(matches(isDisplayed()));
+         onView(withId(R.id.radioGroup)).check(matches(isDisplayed()));
+         onView(withId(R.id.content_name)).check(matches(isDisplayed()));
+         onView(withId(R.id.buttonAdd2)).check(matches(isDisplayed()));
      }
     @Test
     public void testOnSearchCliked()throws Exception{
         onView(withId(R.id.buttonSearch)).perform(click());
-        onView(withId(R.id.delete)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.deleteId)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.text)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.text2)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.editText)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.editText2)).check(matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.delete)).check(matches(isDisplayed()));
+        onView(withId(R.id.deleteId)).check(matches(isDisplayed()));
+        onView(withId(R.id.text)).check(matches(isDisplayed()));
+        onView(withId(R.id.text2)).check(matches(isDisplayed()));
+        onView(withId(R.id.editText)).check(matches(isDisplayed()));
+        onView(withId(R.id.editText2)).check(matches(isDisplayed()));
 
     }
     @Test
     public void testOnAddClickeFromAddCategoryScreen(){
-
+      //the test starts at main screen
+       onView(withId(R.id.buttonAdd)).perform(click());
+        onView(withId(R.id.buttonAdd2)).check(matches(isDisplayed()));
         onView(withId(R.id.buttonAdd2)).perform(click());
         //actualizando test
-        onView(withId(R.id.title)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.recycler)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.image)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.buttonSearch)).check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.buttonAdd)).check(matches(ViewMatchers.isDisplayed()));
+
+        onView(withId(R.id.recycler)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.buttonSearch)).check(matches(isDisplayed()));
+        onView(withId(R.id.buttonAdd)).check(matches(isDisplayed()));
+    }
+@Test
+    public void loadAllCategories()throws Exception{
+
+    }
+    @Test
+    public void testShouldBeAbleToLaunchActivityAndSeeRealmResults() {
+        onView(withText("Música")).check(matches(isDisplayed()));
+         onView(withText("ULPGC")).check(matches(isDisplayed()));
+        onView(withText("Fiestas")).check(matches(isDisplayed()));
+
     }
 
+    @Test
+    public void verifyMessageSentToMainActivity() {
 
-}
+
+        onView(withId(R.id.buttonAdd)).perform(click());
+        // Types a message into a EditText element.
+        onView(withId(R.id.content_name))
+                .perform(typeText(MESSAGE), closeSoftKeyboard());
+
+        // Clicks a button to send the message to another
+        // activity through an explicit intent.
+        onView(withId(R.id.buttonAdd2)).perform(click());
+        onView(withText("This is a test")).check(matches(isDisplayed()));
+        // Verifies that the DisplayMessageActivity received an intent
+        // with the correct package name and message.
+
+    }
+  @Test
+    public void insertEventTest(){
+    final  CategoryModel c= new CategoryModel();
+      RealmConfiguration testConfig =
+              new RealmConfiguration.Builder().
+                      inMemory().
+                      name("test-realm").build();
+
+      Realm testRealm = Realm.getInstance(testConfig);
+
+    c.insertEvent("bkjbbukbujbujbuhb",R.drawable.astro);
+//     testRealm.executeTransaction(new Realm.Transaction() {
+//         @Override
+//         public void execute(Realm realm) {
+//             c.insertEvent("jgbugb",R.drawable.astro);
+//         }
+//     });
+//  }
+}}
+
